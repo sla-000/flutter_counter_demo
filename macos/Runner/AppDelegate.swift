da15fca0222288aa85f60c577818573290cfc992
Vyacheslav Ryabinin <ryabinin.v.v@gmail.com>
2021-11-18 20:30:09 +0300

    Add firebase to macos

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,9 +1,22 @@
 import Cocoa
 import FlutterMacOS
+import UIKit
+import Firebase
 
+@UIApplicationMain
 @NSApplicationMain
-class AppDelegate: FlutterAppDelegate {
+class AppDelegate: FlutterAppDelegate, UIResponder, UIApplicationDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    return true
+  }
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+      [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+
     return true
   }
 }
