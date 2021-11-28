526cb3e19baa5a6143ccf806f4151fa36e719423
Vyacheslav Ryabinin <ryabinin.v.v@gmail.com>
2021-11-28 12:52:37 +0300

    Remove theme bloc - work improperly

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_counter_shooter/di/di.dart';
 import 'package:flutter_counter_shooter/l10n/generated/l10n.dart';
-import 'package:flutter_counter_shooter/theme/bloc.dart';
+import 'package:flutter_counter_shooter/theme/default_theme.dart';
 import 'package:flutter_counter_shooter/theme/durations.dart';
-import 'package:flutter_counter_shooter/theme/event.dart';
-import 'package:flutter_counter_shooter/theme/state.dart';
 import 'package:flutter_counter_shooter/ui/home/main.dart';
 import 'package:flutter_counter_shooter/ui/splash/splash_screen.dart';
 import 'package:flutter_counter_shooter/ui/splash/splash_while_wait_future.dart';
@@ -36,7 +33,6 @@ class _AppState extends State<App> {
       splashFuture: Future.wait<void>(<Future<void>>[
         Future<void>.delayed(const Duration(milliseconds: 1000)),
         Firebase.initializeApp(),
-        Future<void>(() => di.get<ThemeBloc>().add(ThemeEvent.gameOff(context))),
       ]),
       splashScreen: AnimatedSwitcher(
         duration: kXlDuration,
@@ -50,22 +46,17 @@ class _AppState extends State<App> {
       ),
       child: AnimatedSwitcher(
         duration: kXlDuration,
-        child: BlocBuilder<ThemeBloc, ThemeState>(
-          bloc: di.get<ThemeBloc>(),
-          builder: (BuildContext context, ThemeState themeState) {
-            return MaterialApp(
-              onGenerateTitle: (BuildContext context) => context.l10n.flutterCounterDemo,
-              theme: themeState.theme,
-              home: const HomePage(),
-              localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
-                AppLocalizations.delegate,
-                GlobalMaterialLocalizations.delegate,
-                GlobalWidgetsLocalizations.delegate,
-                GlobalCupertinoLocalizations.delegate,
-              ],
-              supportedLocales: AppLocalizations.delegate.supportedLocales,
-            );
-          },
+        child: MaterialApp(
+          onGenerateTitle: (BuildContext context) => context.l10n.flutterCounterDemo,
+          theme: defaultTheme(context),
+          home: const HomePage(),
+          localizationsDelegates: const <LocalizationsDelegate<dynamic>>[
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: AppLocalizations.delegate.supportedLocales,
         ),
       ),
     );
