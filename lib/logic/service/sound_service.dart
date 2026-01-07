db3b60ce7596ead649c928f3785d24d76f7f3928
Slava Ryabinin <ryabinin.v.v@gmail.com>
2026-01-07 19:18:41 +0400

    fix: First shoot sound. Reuse AssetSource instances for sound playback.

--- lib/logic/service/sound_service.dart
+++ lib/logic/service/sound_service.dart
@@ -5,18 +5,22 @@ import 'package:audioplayers/audioplayers.dart';
 class SoundService {
   final AudioPlayer _player = AudioPlayer();
 
+  final _explosion = AssetSource('sounds/explosion.mp3');
+  final _dead = AssetSource('sounds/dead.mp3');
+  final _shoot = AssetSource('sounds/shoot.mp3');
+
   Future<void> load() async {
     await Future.wait([
-      _player.setSource(AssetSource('sounds/explosion.mp3')),
-      _player.setSource(AssetSource('sounds/dead.mp3')),
-      _player.setSource(AssetSource('sounds/shoot.mp3')),
+      _player.setSource(_explosion),
+      _player.setSource(_dead),
+      _player.setSource(_shoot),
     ]);
   }
 
   void playExplosion() {
     unawaited(
       _player.play(
-        AssetSource('sounds/explosion.mp3'),
+        _explosion,
         mode: PlayerMode.lowLatency,
       ),
     );
@@ -24,14 +28,17 @@ class SoundService {
 
   void playDead() {
     unawaited(
-      _player.play(AssetSource('sounds/dead.mp3'), mode: PlayerMode.lowLatency),
+      _player.play(
+        _dead,
+        mode: PlayerMode.lowLatency,
+      ),
     );
   }
 
   void playShoot() {
     unawaited(
       _player.play(
-        AssetSource('sounds/shoot.mp3'),
+        _shoot,
         mode: PlayerMode.lowLatency,
       ),
     );
