f2cd47cda4f2d5c005e03d21c5333f75f66771de
Slava Ryabinin <ryabinin.v.v@gmail.com>
2026-01-09 18:55:53 +0400

    chore: Remove unused logic

--- lib/logic/game/enemy/bomb.dart
+++ lib/logic/game/enemy/bomb.dart
@@ -8,41 +8,20 @@ class Bomb extends ActorMoving implements Updatable {
     required double angle,
     required Vector linearSpeed,
   }) : super(
-          position: position,
-          angle: angle,
-          linearSpeed: linearSpeed,
-          size: Vector.square(size: 50),
-        );
-
-  double animation = 0;
+         position: position,
+         angle: angle,
+         linearSpeed: linearSpeed,
+         size: Vector.square(size: 50),
+       );
 
   @override
   Bomb copyWith({
     required Vector position,
-  }) =>
-      Bomb(
-        position: position,
-        angle: angle,
-        linearSpeed: linearSpeed,
-      );
-
-  @override
-  void update(double delta) {
-    super.update(delta);
-
-    _updateAnimation(delta);
-  }
-
-  void _updateAnimation(double delta) {
-    var calcAnimation = animation;
-
-    calcAnimation += delta * 1.2;
-    while (calcAnimation > 1) {
-      calcAnimation -= 1;
-    }
-
-    animation = calcAnimation;
-  }
+  }) => Bomb(
+    position: position,
+    angle: angle,
+    linearSpeed: linearSpeed,
+  );
 
   @override
   String toString() => 'Bomb{moving: ${super.toString()}}';
